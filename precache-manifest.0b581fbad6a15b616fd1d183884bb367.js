99d77bf2396eaa896a6eaf639c0e8b668cb156db
RofixWork <wourkout123@gmail.com>
2020-10-09 15:03:17 +0100

    Updates

---
+++ precache-manifest.0b581fbad6a15b616fd1d183884bb367.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b54e0882808392b067190d52875175",
+    "url": "/switchColor/index.html"
+  },
+  {
+    "revision": "cef02a56e5cec8f2c603",
+    "url": "/switchColor/static/css/main.412346f2.chunk.css"
+  },
+  {
+    "revision": "edaa75e7d5d68ef51d05",
+    "url": "/switchColor/static/js/2.ee41f74d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/switchColor/static/js/2.ee41f74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cef02a56e5cec8f2c603",
+    "url": "/switchColor/static/js/main.d0081d23.chunk.js"
+  },
+  {
+    "revision": "e3185d9f0a519c294abb",
+    "url": "/switchColor/static/js/runtime-main.608dd350.js"
+  }
+]);
